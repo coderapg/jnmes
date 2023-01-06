74116dd36e29502d1f50a05cd333d20204b43c22
李永峰 <1184156787@qq.com>
2023-01-06 10:44:00 +0800

    配置全局引入element

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 // 路由映射关系表
 import router from './router'
 
+// 全局引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
