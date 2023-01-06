fead9bb608daaebced3bbd23272d457190de777d
李永峰 <1184156787@qq.com>
2023-01-06 14:01:18 +0800

    登录-获取验证码并展示

--- src/https/login.js
+++ src/https/login.js
@@ -1,3 +1,12 @@
 /**
  * login相关模块封装
  */
+import { request } from './request'
+
+// 根据时间戳获取验证码
+export function getVerificationCode (timeStamp) {
+  return request({
+    method: 'GET',
+    url: `/sys/randomImage/${timeStamp}`
+  })
+}
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,13 +5,15 @@
         <div class="logo"></div>
       </el-form-item>
       <el-form-item>
-        <el-input v-model="user.name"></el-input>
+        <el-input v-model="user.username"></el-input>
       </el-form-item>
       <el-form-item>
         <el-input v-model="user.password"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-input v-model="user.code"></el-input>
+        <el-input v-model="user.inputCode"></el-input>
+        <img v-if="showRandCode" :src="randCode" alt="" @click="loadVerificationCode" />
+        <img v-else src="./checkcode.png" @click="loadVerificationCode" />
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">立即创建</el-button>
@@ -21,19 +23,44 @@
 </template>
 
 <script>
+import { getVerificationCode } from 'https/login'
 
 export default {
   name: 'LoginIndex',
   data () {
     return {
       user: {
-        name: '',
+        username: '',
         password: '',
-        code: ''
-      }
+        inputCode: ''
+      },
+      timeStamp: '', // 获取当前的时间戳，用于获取最新验证码和登录时验证返回的code
+      randCode: null, // 后台返回的验证码图片
+      showRandCode: true // 判断404图片是否显示
     }
   },
+  created () {
+    this.loadVerificationCode()
+  },
   methods: {
+    // 获取验证码
+    loadVerificationCode () {
+      this.timeStamp = new Date().getTime() // 获取当前时间戳
+      this.user.inputCode = '' // 每次刷新 / 点击时，需要置空验证码
+      getVerificationCode(this.timeStamp).then(res => {
+        const { data: { message, result, success }, status } = res
+        console.log('err', res)
+        if (status === 200 && success) {
+          this.randCode = result
+          this.showRandCode = true
+        } else {
+          this.$message.error(message)
+          this.showRandCode = false
+        }
+      }).catch(() => {
+        this.showRandCode = false
+      })
+    },
     onSubmit () {
       console.log('submit!')
     }
