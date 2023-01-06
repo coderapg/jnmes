69def2fce39f1c1ac39afab49302427bcae15c46
李永峰 <1184156787@qq.com>
2023-01-06 20:44:15 +0800

    统一添加token

--- src/https/request.js
+++ src/https/request.js
@@ -1,8 +1,10 @@
 // 二次封装axios
 import axios from 'axios'
 
+import { JNMES_TOKEN } from 'utils/jsmesconst'
+
 export function request (config) {
-  // 配置公共信息
+  // 配置公共路径信息
   const instance = axios.create({
     baseURL: 'https://www.jnsms.cn',
     // baseURL: 'http://192.168.124.14:8080',
@@ -10,14 +12,19 @@ export function request (config) {
   })
 
   // 添加请求拦截器
-  axios.interceptors.request.use(req => {
+  instance.interceptors.request.use(req => {
+    const token = window.localStorage.getItem(JNMES_TOKEN)
+    // 统一设置token
+    if (token) {
+      instance.headers['X-Access-Token'] = token
+    }
     return req
   }, err => {
     return Promise.reject(err)
   })
 
   // 添加响应拦截器
-  axios.interceptors.response.use(res => {
+  instance.interceptors.response.use(res => {
     // 在2xx范围内的任何状态代码都会触发此函数
     return res
   }, err => {
