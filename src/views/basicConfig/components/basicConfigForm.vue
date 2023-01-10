d8530b268bcd90fc8c06bdbc385772e5a1f43341
李永峰 <1184156787@qq.com>
2023-01-10 15:36:20 +0800

    基础配置-添加表单验证

--- src/views/basicConfig/components/basicConfigForm.vue
+++ src/views/basicConfig/components/basicConfigForm.vue
@@ -2,7 +2,6 @@
   <div class="basic-config-mode">
     <el-dialog
       width="800px"
-      :title="title"
       :visible="dialogVisible"
       :fullscreen="toggleChange"
       append-to-body
@@ -14,21 +13,21 @@
         </div>
       </template>
       <!-- 表单区域 -->
-      <el-form ref="form" :model="form" label-width="80px" :disabled="disabled">
-        <el-form-item label="配置编号">
+      <el-form ref="ruleForm" :model="form" :rules="rules" label-width="80px" :disabled="disabled">
+        <el-form-item label="配置编号" prop="code">
           <el-input v-model="form.code" disabled />
         </el-form-item>
-        <el-form-item label="配置值">
+        <el-form-item label="配置值" prop="value">
           <el-input v-model="form.value" />
         </el-form-item>
-        <el-form-item label="说明">
+        <el-form-item label="配置说明" prop="note">
           <el-input type="textarea" v-model="form.note" disabled />
         </el-form-item>
       </el-form>
       <!-- /表单区域 -->
       <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="submitForm">确定</el-button>
+        <el-button :disabled="disabled" @click="dialogVisible = false">取消</el-button>
+        <el-button :disabled="disabled" type="primary" @click="submitForm">确定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -45,6 +44,17 @@ export default {
       dialogVisible: false,
       disabled: false,
       form: {},
+      rules: {
+        code: [
+          { required: true, message: '请输入配置编号', trigger: 'blur' }
+        ],
+        value: [
+          { required: true, message: '请输入配置值', trigger: 'blur' }
+        ],
+        note: [
+          { required: true, message: '请输入配置说明', trigger: 'blur' }
+        ]
+      },
       toggleChange: false,
       url: {
         add: '/report/businessReport/add',
@@ -70,29 +80,34 @@ export default {
     },
     // 提交
     submitForm () {
-      if (this.form.id) {
-        // 编辑
-        editConfig(this.form).then(res => {
-          const { message, success } = res.data
-          if (success) {
-            this.$message({
-              message,
-              type: 'success'
-            })
-            this.dialogVisible = false
-            // 子组件调用父组件中的方法
-            // this.$parent.loadTableData(1)
-            this.$emit('handleEditChange')
-          } else {
-            this.$message({
-              message,
-              type: 'error'
-            })
-          }
-        })
-      } else {
-        // 新增
-      }
+      this.$refs.ruleForm.validate((valid) => {
+        if (!valid) {
+          return
+        }
+        if (this.form.id) {
+          // 编辑
+          editConfig(this.form).then(res => {
+            const { message, success } = res.data
+            if (success) {
+              this.$message({
+                message,
+                type: 'success'
+              })
+              this.dialogVisible = false
+              // 子组件调用父组件中的方法
+              // this.$parent.loadTableData(1)
+              this.$emit('handleEditChange')
+            } else {
+              this.$message({
+                message,
+                type: 'error'
+              })
+            }
+          })
+        } else {
+          // 新增
+        }
+      })
     }
     // 查看
   }
